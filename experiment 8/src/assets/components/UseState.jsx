a7c6da8bf51839d64732333ed4ac82f5e90b86c2
aayushi64 <aayushi.25b01011187@abes.ac.in>
2026-05-26 21:58:25 +0530

    \

--- experiment 8/src/assets/components/UseState.jsx
+++ experiment 8/src/assets/components/UseState.jsx
@@ -1,24 +1,19 @@
 import React, { useState } from 'react'
 
 function UseState() {
-    const[counter,setCounter]=useState(0);
+  const [counter, setCounter] = useState(0)
 
-    const increment=()=>{
-        setCounter(counter+1);
-    }
-    const decrement=()=>{
-        setCounter(counter-1);
-    }
-    const reset=()=>{
-        setCounter(0);
-    }
+  const increment = () => setCounter(counter + 1)
+  const decrement = () => setCounter(counter - 1)
+  const reset = () => setCounter(0)
+
   return (
-   <>
-   <h1>{counter}</h1>
-   <button onClick={increment} style={{width:'200px', backgroundColor:'blue'}}>+</button>
-  <button onClick={decrement} style={{width:'200px', backgroundColor:'blue', margin:'10px 0px'}}>-</button>
-   <button onClick={reset} style={{width:'200px', backgroundColor:'blue'}}>reset</button> 
-   </>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px' }}>
+      <h1>{counter}</h1>
+      <button onClick={increment} style={{ width: '200px', backgroundColor: 'blue', color: 'white', margin: '5px' }}>+</button>
+      <button onClick={decrement} style={{ width: '200px', backgroundColor: 'blue', color: 'white', margin: '5px' }}>-</button>
+      <button onClick={reset} style={{ width: '200px', backgroundColor: 'blue', color: 'white', margin: '5px' }}>reset</button>
+    </div>
   )
 }
 
