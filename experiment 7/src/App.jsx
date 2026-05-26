ca4fec5b8045586a735028fee3a0720f07b801ab
aayushi64 <aayushi.25b01011187@abes.ac.in>
2026-05-26 20:14:13 +0530

    Student info using react

--- experiment 7/src/App.jsx
+++ experiment 7/src/App.jsx
@@ -3,6 +3,7 @@ import Props from "./assets/components/Props";
 
 function App() {
   return (
+    <>
     <div className="main-box">
       <h1>Student Results</h1>
       <div className="container">
@@ -10,7 +11,12 @@ function App() {
         <Props name="Pihu Sharma" course="Computer Science" marks="92" />
         <Props name="Girish" course="Computer Science" marks="98" />
       </div>
+      <div className="footer-info">
+          <h3>Aayushi Sharma</h3>
+          <h4>Roll Number: 2503201000022</h4>
+        </div>
     </div>
+   </>
   );
 }
 
